0fd45c251c0ff9702c875630372a862820fea406
volvad <volgvad@gmail.com>
2022-10-04 01:00:44 +0400

    Edit js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/project/index.html')
+})
+
+let port = 3000
+app.listen(port, () => {
+   console.log(`Server started: http://localhost:${port}`)
+})
 
